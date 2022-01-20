230736f808c0771867472ee72509a5eddd33ffde
Nyasha1998 <blessedmanox@gmail.com>
2022-01-20 14:27:57 +0200

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get((url), {
+    headers: {
+        'x-rapidapi-host': 'bayut.p.rapidapi.com',
+        'x-rapidapi-key': '66282deb81msh2248264b836b17dp1bd475jsn8607513a839a'
+      }
+});
+
+  return data;
+}
